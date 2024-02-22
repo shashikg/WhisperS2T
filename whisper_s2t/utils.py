5d59195f02e5db919a21b7693c018925b3dc11ef
Shashi Kant <shashikg.iitk@gmail.com>
2024-02-22 00:22:25 +0000

    single sent in utt - txt writer

--- whisper_s2t/utils.py
+++ whisper_s2t/utils.py
@@ -50,7 +50,44 @@ def format_timestamp(seconds, always_include_hours=False, decimal_marker="."):
     )
 
 
-def ExportVTT(transcript, file):
+def get_single_sentence_in_one_utterance(transcript, end_punct_marks=["?", "."]):
+    if 'word_timestamps' not in transcript[0]:
+        print(f"Word Timestamp not available, one utterance can have multiple sentences.")
+        return transcript
+
+    new_transcript = []
+        
+    all_words = []
+    for utt in transcript:
+        all_words += utt['word_timestamps']
+
+    curr_utt = []
+    for word in all_words:
+        curr_utt.append(word)
+        if len(word['word']) and word['word'][-1] in end_punct_marks:
+            if len(curr_utt):
+                new_transcript.append({
+                    'text': " ".join([_['word'] for _ in curr_utt]),
+                    'start_time': curr_utt[0]['start'],
+                    'end_time': curr_utt[-1]['end']
+                })
+
+                curr_utt = []
+
+    if len(curr_utt):
+        new_transcript.append({
+            'text': " ".join([_['word'] for _ in curr_utt]),
+            'start_time': curr_utt[0]['start'],
+            'end_time': curr_utt[-1]['end']
+        })
+
+    return new_transcript
+
+
+def ExportVTT(transcript, file, single_sentence_in_one_utterance=False, end_punct_marks=["?", "."]):
+
+    if single_sentence_in_one_utterance:
+        transcript = get_single_sentence_in_one_utterance(transcript, end_punct_marks=end_punct_marks)
 
     with open(file, 'w') as f:
         f.write("WEBVTT\n\n")
@@ -58,7 +95,10 @@ def ExportVTT(transcript, file):
             f.write(f"{format_timestamp(_utt['start_time'])} --> {format_timestamp(_utt['end_time'])}\n{_utt['text']}\n\n")
 
 
-def ExportSRT(transcript, file):
+def ExportSRT(transcript, file, single_sentence_in_one_utterance=False, end_punct_marks=["?", "."]):
+
+    if single_sentence_in_one_utterance:
+        transcript = get_single_sentence_in_one_utterance(transcript, end_punct_marks=end_punct_marks)
 
     with open(file, 'w') as f:
         for i, _utt in enumerate(transcript):
@@ -75,7 +115,11 @@ def ExportJSON(transcript, file):
         f.write(json.dumps(transcript))
 
 
-def ExportTSV(transcript, file):
+def ExportTSV(transcript, file, single_sentence_in_one_utterance=False, end_punct_marks=["?", "."]):
+
+    if single_sentence_in_one_utterance:
+        transcript = get_single_sentence_in_one_utterance(transcript, end_punct_marks=end_punct_marks)
+
     keys = ['start_time', 'end_time', 'text']
     if len(transcript):
         for k in transcript[0].keys():
@@ -87,7 +131,18 @@ def ExportTSV(transcript, file):
             f.write("\n" + "\t".join([_utt[k].strip().replace("\t", " ") if k == 'text' else str(_utt[k]) for k in keys]))
 
 
+def ExportTXT(transcript, file, single_sentence_in_one_utterance=False, end_punct_marks=["?", "."]):
+
+    if single_sentence_in_one_utterance:
+        transcript = get_single_sentence_in_one_utterance(transcript, end_punct_marks=end_punct_marks)
+
+    with open(file, 'w') as f:
+        for _utt in transcript:
+            f.write(f"[{format_timestamp(_utt['start_time'])} --> {format_timestamp(_utt['end_time'])}]: {_utt['text']}\n\n")
+
+
 TranscriptExporter = {
+    'txt': ExportTXT,
     'vtt': ExportVTT,
     'srt': ExportSRT,
     'tsv': ExportTSV,
@@ -95,7 +150,7 @@ TranscriptExporter = {
 }
 
 
-def write_outputs(transcripts, format='json', ip_files=None, op_files=None, save_dir="./"):
+def write_outputs(transcripts, format='json', ip_files=None, op_files=None, save_dir="./", **kwargs):
     if (op_files is None) or (len(op_files) != len(transcripts)):
         os.makedirs(save_dir, exist_ok=True)
 
@@ -111,4 +166,4 @@ def write_outputs(transcripts, format='json', ip_files=None, op_files=None, save
 
     
     for transcript, file_name in zip(transcripts, op_files):
-        TranscriptExporter[format](transcript, file_name)
+        TranscriptExporter[format](transcript, file_name, **kwargs)
