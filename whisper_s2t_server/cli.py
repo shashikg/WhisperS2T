edbce76cec743ac520f00fcd45ff49862ee78a23
Shashi Kant <shashikg.iitk@gmail.com>
2024-08-26 19:58:49 +0000

    minor

--- whisper_s2t_server/cli.py
+++ whisper_s2t_server/cli.py
@@ -9,7 +9,7 @@ from .logger import LogFileHandler, StreamLogs
 
 
 def start_server(server_port, asr_args, vad_args, app_args):
-    shutil.rmtree(f"{WHISPER_S2T_SERVER_TMP_PATH}/")
+    # shutil.rmtree(f"{WHISPER_S2T_SERVER_TMP_PATH}/")
     os.makedirs(f'{WHISPER_S2T_SERVER_TMP_PATH}/logs', exist_ok=True)
     
     # Start the Gunicorn server
@@ -26,24 +26,24 @@ def start_server(server_port, asr_args, vad_args, app_args):
     ]
     
     with open(f'{WHISPER_S2T_SERVER_TMP_PATH}/logs/rest_server.log', 'a') as log_file:
-        rest_server_process = subprocess.Popen(rest_server_command, stdout=log_file, stderr=log_file)
+        rest_server_process = subprocess.Popen(rest_server_command, stdout=log_file, stderr=subprocess.STDOUT)
     
     # Start the ASR process
     asr_input_args = [f"--{k}={v}" for k, v in asr_args.items()]
     asr_command = ["python3", "-m", "whisper_s2t_server.models.asr.main"] + asr_input_args
     with open(f'{WHISPER_S2T_SERVER_TMP_PATH}/logs/asr.log', 'a') as log_file:
-        asr_process = subprocess.Popen(asr_command, stdout=log_file, stderr=log_file)
+        asr_process = subprocess.Popen(asr_command, stdout=log_file, stderr=subprocess.STDOUT)
     
     # Start the VAD process
     vad_input_args = [f"--{k}={v}" for k, v in vad_args.items()]
     vad_command = ["python3", "-m", "whisper_s2t_server.models.vad.main"] + vad_input_args
     with open(f'{WHISPER_S2T_SERVER_TMP_PATH}/logs/vad.log', 'a') as log_file:
-        vad_process = subprocess.Popen(vad_command, stdout=log_file, stderr=log_file)
+        vad_process = subprocess.Popen(vad_command, stdout=log_file, stderr=subprocess.STDOUT)
 
     # Start Main Worker
     main_worker_command = ["python3", "-m", "whisper_s2t_server.worker"]
     with open(f'{WHISPER_S2T_SERVER_TMP_PATH}/logs/main_worker.log', 'a') as log_file:
-        main_worker_process = subprocess.Popen(main_worker_command, stdout=log_file, stderr=log_file)
+        main_worker_process = subprocess.Popen(main_worker_command, stdout=log_file, stderr=subprocess.STDOUT)
 
     # Start the Streamlit App process
     app_input_args = [
@@ -54,7 +54,7 @@ def start_server(server_port, asr_args, vad_args, app_args):
     ]
     app_command = ["streamlit", "run", f"{BASE_PATH}/app.py"] + app_input_args
     with open(f'{WHISPER_S2T_SERVER_TMP_PATH}/logs/app.log', 'a') as log_file:
-        app_process = subprocess.Popen(app_command, stdout=log_file, stderr=log_file)
+        app_process = subprocess.Popen(app_command, stdout=log_file, stderr=subprocess.STDOUT)
     
     # Save PIDs to a file
     with open(f'{WHISPER_S2T_SERVER_TMP_PATH}/logs/process_pids.txt', 'w') as pid_file:
